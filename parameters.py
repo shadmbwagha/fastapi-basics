31ecc84b40046de77b498b440c8f46d57acbdb4a
shad <105544140+shadmbwagha@users.noreply.github.com>
2025-05-10 11:19:51 +0300

    params

--- parameters.py
+++ parameters.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Annotated
 
+from fastapi import Query
 
 class Item(BaseModel):
     name: str
@@ -18,3 +20,20 @@ async def update_item(item_id: int, item: Item, q: str | None = None):
     if q:
         result.update({"q": q})
     return result
+
+# @app.get("/items/")
+# async def read_items(q: str | None = None):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+
+
+@app.get("/items/")
+async def read_items(q : Annotated[str | None, Query( min_length=3, max_length=50)]):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
